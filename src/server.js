98c887729cc0aa508efe2b90f1086fbd85acc12a
Felipe <felipecollyer@gmail.com>
2024-06-14 21:16:26 -0300

    add .env port

--- src/server.js
+++ src/server.js
@@ -1,14 +1,21 @@
+import 'dotenv/config';
 import createApp from './app.js';
 import dbServer from './config/database.js';
+
+const port = process.env.SERVER_PORT
 
 const start = async () => {
   const app = createApp();
 
-  try {  
-    await app.listen({ port: 3000 });
-    dbServer.$connect().then(()=> console.log(`conectado ao DB`))
+  try {
+    await app.listen({ port });
+    console.log(`Servidor conectado na porta ${port}`);
+
+    await dbServer.$connect();
+    console.log('Conectado ao DB');
+
   } catch (err) {
-    app.log.error(err);
+    console.error('Erro ao iniciar o servidor ou conectar ao banco de dados:', err);
     process.exit(1);
   }
 };
